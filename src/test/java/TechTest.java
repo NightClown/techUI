4e0916f4cb59e06c8c355a2b0e87584299f00053
NightClown <233751356@qq.com>
2019-03-05 14:10:11 +0800

    docker practice

--- src/test/java/TechTest.java
+++ src/test/java/TechTest.java
@@ -1,15 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.impl.WebDriverContainer;
 import com.codeborne.selenide.impl.WebDriverThreadLocalContainer;
-import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.switchTo;
 
 /**
  * Created by sungaofei on 19/2/28.
@@ -23,9 +19,9 @@ public class TechTest {
         webdriverContainer.clearBrowserCache();
 //
         Configuration.browser="chrome";
-        Configuration.remote = "http://39.105.132.200:5001/wd/hub";
+        Configuration.remote = "http://nightclown.tk:5001/wd/hub";
 
-        String baseUrl = "http://jenkins.testing-studio.com:8999/";
+        String baseUrl = "http://nightclown.tk:5000/";
 
         com.codeborne.selenide.Selenide.open(baseUrl);
 
